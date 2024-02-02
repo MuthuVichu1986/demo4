a0921d074d046bb9380b1524d1adefa7043c862f
MuthuVichu1986 <101440553+MuthuVichu1986@users.noreply.github.com>
2024-02-02 08:50:45 +0000

    Add files via upload

--- New.java
+++ New.java
@@ -1,42 +1,30 @@
-package learn;
-
-import java.util.Set;
+package Testng;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class New {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       WebDriver driver= new ChromeDriver();
-       driver.get("https://demoqa.com/browser-windows");
-       //String mainWindow= driver.getWindowHandle();
-       WebElement newWindowButton= driver.findElement(By.id("windowsButton"));
-       newWindowButton.click();
-       Set<String> windowHandles = driver.getWindowHandles();
-       for (String windowHandle :windowHandles) {
-    	   driver.switchTo().window(windowHandle);
-       }
-       WebElement newWindowText= driver.findElement(By.id("sampleHandling"));
-       System.out.println("Text in the window:" + newWindowText.getText());
-       driver.close();
-       driver.switchTo().window(windowHandles.iterator().next());
-       WebElement newTabButton = driver.findElement(By.id("tabButton"));
-       newTabButton.click();
-       windowHandles = driver.getWindowHandles();
-       for (String windowHandle :windowHandles) {
-    	   driver.switchTo().window(windowHandle);
-    	   
-       }
-       WebElement newTabText = driver.findElement(By.id("sampleHeading"));
-       System.out.println("Text in the new Tab" + newTabText.getText());
-       driver.quit();
-       
-       
-       
-	}
+      WebDriver driver= new ChromeDriver();
+      driver.get("https://demo.automationtesting.in/Register.html");
+      driver.manage().window().maximize();
+      driver.findElement(By.cssSelector("input[placeholder='First Name']")).sendKeys("MUTHU");
+      driver.findElement(By.cssSelector("input[placeholder='Last Name']")).sendKeys("KUMAR");
+      driver.findElement(By.xpath("//textarea[@rows='3']")).sendKeys("123ghgh");
+      driver.findElement(By.cssSelector("input[type='Email']")).sendKeys("Ad@gmail.com");
+      driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("6789456123");
+      driver.findElement(By.xpath("//input[@type='radio']")).click();
+      driver.findElement(By.xpath("//input[@name='radiooptions']")).click();
+      driver.findElement(By.id("checkbox1")).click();
+      driver.findElement(By.id("checkbox2")).click();
+      driver.findElement(By.id("checkbox3")).click();
+      
+      
+      
+	
 
+}
 }
